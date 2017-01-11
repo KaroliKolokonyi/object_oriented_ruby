bde59b47f081032c979c4823a7756d0e779cec70
Karoli Kolokonyi <karoli@kepler.org>
2017-01-11 14:52:55 +0200

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+product1 = {"Product_Name" =>"Computer", "Brand" =>"Lenovo", "color"=>"Red", "Price" =>1500}
+
+product2 = {"Product_Name" =>"TV", "Brand" => "Konka", "color"=>"Black", "Price" =>3000}
+
+product3 = {"Product_Name" => "Refrigirator", "Brand" => "Sumsung", "color" =>"white", "Price" => 1500}
+
+puts "The brand for a refrigirator is  #{product3["Brand"]}"
