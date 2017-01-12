7fbb8835347b71cdd30da785a9e3b9e1e101a452
Karoli Kolokonyi <karoli@kepler.org>
2017-01-12 11:37:22 +0200

    Third commit

--- products.rb
+++ products.rb
@@ -1,24 +1,18 @@
-product1 = {"Product_Name" =>"Computer", "Brand" =>"Lenovo", "color"=>"Red", "Price" =>1500}
-
-product2 = {"Product_Name" =>"TV", "Brand" => "Konka", "color"=>"Black", "Price" =>3000}
-
-product3 = {"Product_Name" => "Refrigirator", "Brand" => "Sumsung", "color" =>"white", "Price" => 1500}
-
-puts "The brand for a refrigirator is  #{product3["Brand"]}"
-
-
 class Product 
 
-	def initialize (product_name, product_brand, product_color, product_price)
+	def initialize (product_name, product_brand, product_price)
 		@name = product_name 
 		@brand = product_brand
-		@color = product_color
 		@price = product_price
 
 	end 
 
 	def name
 		@name 
+	end 
+
+	def brand 
+		@brand 
 	end 
 
 
@@ -28,15 +22,14 @@ class Product
 
 	def price=(product_price)
 		@price = product_price
-    end 
+	end 
     
-    def info
-
-     "#{name}, #{@color} #{@brand} #{@price}"
-    end 
-
+  
 end
 
-product = Product.new("Computer","Lenovo","Black",1500)
-puts info 
+product = Product.new("Computer","Lenovo",1500)
+puts product.brand 
+puts product.name 
+product.price = 5000
+puts product.price 
 
