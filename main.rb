7a714b44d32935d35ffe05cd031281be499f7c02
Karoli Kolokonyi <karoli@kepler.org>
2017-01-13 12:06:27 +0200

    separating arguments by saving them into different files

---
+++ main.rb
@@ -0,0 +1,8 @@
+require "./product2.rb"
+
+product = Products.new ({product_name: "Computer", brand_name: "Lenovo", price: 20000})
+puts product.info
+puts product.product_name 
+puts product.brand_name
+product.price = 25000
+puts product.price
---
+++ product2.rb
@@ -0,0 +1,16 @@
+
+class Products 
+attr_reader :product_name, :brand_name, :price 
+attr_writer :price 
+
+def initialize(product_hash)
+    @product_name = product_hash[:product_name]
+    @brand_name = product_hash[:brand_name]
+    @price = product_hash[:price]
+  end
+
+def info
+     "#{@product_name} #{@brand_name} whose price is #{@price}"
+ end
+
+end 
